b418617cfe41456064263df1fa9740aec57b3bf7
AJLosey <aaronlosey11@gmail.com>
2022-06-05 18:34:48 -0500

    starting transition to react, created index.js and top-level react function

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Page from './Page';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<Page />, document.getElementById('root'));
