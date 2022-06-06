d06fa2ec8f430d3f0bbcc8722e618ef1b279af09
AJLosey <aaronlosey11@gmail.com>
2022-06-05 23:36:56 -0500

    resolved project cards to a component

---
+++ components/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function About() {
+    <div class="container purp-text ">
+        <div class="col-3">
+            <p class="h4" id="about-me">About Me</p>
+        </div>
+        <div class="row purp-bg py-3">
+            <div class="col-3"><img src="./images/photo2.jpg" alt="photo of me. It's 80% beard"
+                class="col-12 purp-border px-0" /></div>
+            <div class="col-9">
+                <p>I highly recommend that everyone listen to the 2016 album "Spirit Phone"
+                    by Lemon Demon. There's a song called "Cabinet Man" about a living arcade machine that has two lines
+                    that reference the game "Polybius". For those that don't know, Polybius is an equally fictional
+                    arcade
+                    machine allegedly deployed by some deep-government-psy-ops that drove people crazy. "Lemon Demon,"
+                    by
+                    the
+                    way, is a band with only one official member, the musical genius called Neil Cicierega. Another one
+                    of
+                    my favorite songs in the album is "Spiral of Ants". This masterpiece describes a real phenomenon
+                    where
+                    ants accidentally lay down guiding pheremones in a circle. Ants can get trapped in this circle,
+                    following their own trails, and laying down more pheremones as they go. Other ants get sucked in,
+                    the problem worsens, and eventually massive swarms can get caught up in this lethal feedback loop.
+                    It's
+                    insane, fascinating, and the song makes me think of the future of our own species. <br></br> As for more official information about myself, I am a student in the
+                    full-stack development bootcamp at
+                    University of Texas. I
+                    live in the South Austin
+                    area. I enjoy javascript and c#, and have some familiarity with html, css, jquery, and bootstrap.
+                    When
+                    I'm not learning code, I'm either learning something about biology, learning about or playing video
+                    games, writing, or I'm asleep. </p>
+            </div>
+        </div>
+    </div>
+}
---
+++ components/Navbar.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+function Nav() {
+    <nav class="navbar">
+        <div class="container-fluid">
+            <span class="navbar-text">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-4">
+                        <h1 class="h3">
+                            Portfolio for Aaron Losey
+                        </h1>
+                    </div>
+                    <div class="col-2">
+                        <a href="https://drive.google.com/file/d/1_Sa2hdqHL4J_7zCmmWZPTEzb9DD15czZ/view?usp=sharing"
+                            target="_blank" rel="noopener noreferrer">Resume Link</a>
+                    </div>
+                </div>
+            </span>
+        </div>
+    </nav>
+}
